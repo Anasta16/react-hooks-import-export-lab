1befeb9357d8a97f27961eac9dd7f9b7fdfa21df
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-01-31 13:59:58 -0500

    import export

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import About from './About'
+import Home from './Home'
+import NavBar from './NavBar'
+import user from '../data/user'
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.username} city={user.city}/>
+      <About image={user.image}/>
     </div>
   );
 }
